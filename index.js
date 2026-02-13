28eb14f99e5b23bb839ccdfe2c2a50fda6084d7c
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-02-13 15:49:27 +0530

    MOngobd Delta

---
+++ index.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+
+main().then((res) =>{
+    console.log("Connection succesful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model('User', userSchema);
+
+/**const user2 = new User({
+    name: "Deep",
+    email: "deep@example.com",
+    age: 21
+});
+
+user2.save().then((res) => {
+    console.log(res);
+})
+ .catch((err) =>{
+    console.log(err);
+});
+**/
+
+
+
+//const Employee = mongoose.model("Employee" , userSchema);
+
+
+/**User.insertMany([
+    {
+    name: "anshika",
+    email: "anshika@example.com",
+    age: 21
+},
+{
+    name: "komal",
+    email: "komal@example.com",
+    age: 25
+},{
+    name: "Jane",
+    email: "jane@example.com",
+    age: 28
+}
+    
+]).then((res) =>{
+    console.log(res)
+});
+**/
+
+User.findOne({age: {$gte: 25}})
+.then((res) => {
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err);
+});
